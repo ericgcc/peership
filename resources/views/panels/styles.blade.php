f43d90846e5986038a156c0fda005c4618a6ca47
ericgcc <ericgcc@gmail.com>
2023-01-07 02:54:39 -0500

    pkg: add streamline webfont to the template

--- resources/views/panels/styles.blade.php
+++ resources/views/panels/styles.blade.php
@@ -5,6 +5,7 @@
   <link rel="stylesheet" href="{{ asset(mix('vendors/css/vendors.min.css')) }}" />
   <link rel="stylesheet" href="{{asset(mix('fonts/fontawesome/css/fontawesome.min.css'))}}">
   <link rel="stylesheet" href="{{asset(mix('fonts/fontawesome/css/light.min.css'))}}">
+  <link rel="stylesheet" href="{{asset(mix('fonts/streamline/style.css'))}}">
 @endif
 
 @yield('vendor-style')

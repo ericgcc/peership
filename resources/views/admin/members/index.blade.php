ad4661ff21ef363d8850e0a2d6c6c5e9f1dbd4fb
ericgcc <ericgcc@gmail.com>
2023-01-17 12:15:33 -0500

    core: remove unused imports on view

--- resources/views/admin/members/index.blade.php
+++ resources/views/admin/members/index.blade.php
@@ -4,13 +4,10 @@
 
 @section('vendor-style')
     <!-- vendor css files -->
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
 @endsection
 
 @section('page-style')
     <!-- Page css files -->
-    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-wizard.css')) }}">
 @endsection
 
 @section('content')
@@ -34,10 +31,7 @@
 
 @section('vendor-script')
     <!-- vendor files -->
-    <script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/validation/jquery.validate.min.js')) }}"></script>
 @endsection
 @section('page-script')
     <!-- Page js files -->
-    <script src="{{ asset(mix('js/scripts/forms/form-wizard.js')) }}"></script>
 @endsection

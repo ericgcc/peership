22633895f5412b5b7010e5ea8792d4744861d0b4
ericgcc <ericgcc@gmail.com>
2023-01-17 12:25:18 -0500

    core: add members create form fields

--- resources/views/admin/members/create.blade.php
+++ resources/views/admin/members/create.blade.php
@@ -6,20 +6,21 @@
     <!-- vendor css files -->
     <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/intl-tel-input/intlTelInput.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/webui-popover/jquery.webui-popover.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/peership/css/forms/intl-tel-input/intlTelInput.min.css')) }}">
+    <link rel="stylesheet"
+          href="{{ asset(mix('vendors/peership/css/forms/webui-popover/jquery.webui-popover.min.css')) }}">
 @endsection
 
 @section('page-style')
     <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/pickers/form-flat-pickr.css')) }}">
     <style>
         .iti__flag {
-            background-image: url("{{asset('vendors/css/forms/intl-tel-input/img/flags.png')}}");
+            background-image: url("{{ asset(mix('vendors/peership/css/forms/intl-tel-input/img/flags.png')) }}");
         }
 
         @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
             .iti__flag {
-                background-image: url("{{asset('vendors/css/forms/intl-tel-input/img/flags@2x.png.png')}}");
+                background-image: url("{{ asset(mix('vendors/peership/css/forms/intl-tel-input/img/flags@2x.png')) }}");
             }
         }
 
@@ -35,7 +36,17 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <form class="form">
+                    <form class="form" method="POST" action="{{route('members.store')}}">
+                        @csrf
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
                         <div class="card-header">
                             <h4 class="card-title">Basic Info</h4>
                         </div>
@@ -52,9 +63,10 @@
                                         <input
                                             type="text"
                                             id="first-name"
+                                            name="first-name"
                                             class="form-control"
                                             placeholder="First Name"
-                                            name="first-name"
+                                            value="{{old('first-name')}}"
                                         />
                                     </div>
                                 </div>
@@ -65,9 +77,10 @@
                                         <input
                                             type="text"
                                             id="last-name"
+                                            name="last-name"
                                             class="form-control"
                                             placeholder="Last Name"
-                                            name="last-name"
+                                            value="{{old('last-name')}}"
                                         />
                                     </div>
                                 </div>
@@ -83,22 +96,24 @@
                                         <input
                                             type="text"
                                             id="preferred-name"
+                                            name="preferred-name"
                                             class="form-control"
                                             placeholder="Preferred Name"
-                                            name="preferred-name"
+                                            value="{{old('preferred-name')}}"
                                         />
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
-                                        <label class="form-label" for="email-id-column">Email<strong><span
+                                        <label class="form-label" for="email">Email<strong><span
                                                     class="text-danger">*</span></strong></label>
                                         <input
                                             type="email"
-                                            id="email-id-column"
+                                            id="email"
+                                            name="email"
                                             class="form-control"
-                                            name="email-id-column"
                                             placeholder="Email"
+                                            value="{{old('email')}}"
                                         />
                                     </div>
                                 </div>
@@ -109,9 +124,10 @@
                                         <input
                                             type="tel"
                                             id="primary-phone"
-                                            class="form-control"
                                             name="primary-phone"
+                                            class="form-control"
                                             style="width: 100%"
+                                            value="{{old('primary-phone')}}"
                                         />
                                     </div>
                                 </div>
@@ -121,9 +137,10 @@
                                         <input
                                             type="tel"
                                             id="secondary-phone"
-                                            class="form-control"
                                             name="secondary-phone"
+                                            class="form-control"
                                             style="width: 100%"
+                                            value="{{old('secondary-phone')}}"
                                         />
                                     </div>
                                 </div>
@@ -135,14 +152,15 @@
                                     <div class="mb-1">
                                         <label class="form-label" for="country">Country of origin<strong><span
                                                     class="text-danger">*</span></strong></label>
-                                        <select class="select2 form-select" id="country"></select>
+                                        <select class="form-select select2" id="country" name="country"></select>
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="language">Language for
                                             communications<strong><span class="text-danger">*</span></strong></label>
-                                        <select class="select2 form-select select2-language" id="language">
+                                        <select class="select2 form-select select2-language" id="language"
+                                                name="language">
                                             <option></option>
                                             <option value="en">English</option>
                                             <option value="fr">French</option>
@@ -158,25 +176,34 @@
                                            data-role="tooltip"
                                            data-content="Refers to the current gender, which may be different from the gender assigned at birth."
                                            data-trigger="hover"></i>
-                                        <select class="select2 form-select" id="gender">
+                                        <select class="select2 form-select" id="gender" name="gender">
                                             <option></option>
-                                            <option value="1">Cis man</option>
-                                            <option value="2">Cis woman</option>
-                                            <option value="3">Trans man</option>
-                                            <option value="4">Trans woman</option>
-                                            <option value="5">Non-binary</option>
-                                            <option value="6">Other</option>
+                                            <option value="1" {{old('pronouns') == "1" ? "selected":"" }}>Cis man
+                                            </option>
+                                            <option value="2" {{old('pronouns') == "2" ? "selected":"" }}>Cis woman
+                                            </option>
+                                            <option value="3" {{old('pronouns') == "3" ? "selected":"" }}>Trans man
+                                            </option>
+                                            <option value="4" {{old('pronouns') == "4" ? "selected":"" }}>Trans woman
+                                            </option>
+                                            <option value="5" {{old('pronouns') == "5" ? "selected":"" }}>Non-binary
+                                            </option>
+                                            <option value="6" {{old('pronouns') == "6" ? "selected":"" }}>Other</option>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="pronouns">Pronouns</label>
-                                        <select class="select2 form-select" id="pronouns">
+                                        <select class="select2 form-select" id="pronouns" name="pronouns">
                                             <option></option>
-                                            <option value="1">He/Him/His</option>
-                                            <option value="2">She/Her/Hers</option>
-                                            <option value="3">They/Them/Theirs</option>
+                                            <option value="1" {{old('pronouns') == "1" ? "selected":"" }}>He/Him/His
+                                            </option>
+                                            <option value="2" {{old('pronouns') == "2" ? "selected":"" }}>She/Her/Hers
+                                            </option>
+                                            <option value="3" {{old('pronouns') == "3" ? "selected":"" }}>
+                                                They/Them/Theirs
+                                            </option>
                                         </select>
                                     </div>
                                 </div>
@@ -184,14 +211,15 @@
                                     <div class="mb-1">
                                         <label class="form-label" for="birth-date">Date of birth<strong><span
                                                     class="text-danger">*</span></strong></label>
-                                        <input type="text" id="birth-date" class="form-control flatpickr-basic"
-                                               placeholder="January 1, 2000"/>
+                                        <input type="text" id="birth-date" name="birth-date"
+                                               class="form-control flatpickr-basic"
+                                               placeholder="January 1, 2000" value="{{old('birth-date')}}"/>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="card-header">
-                            <h4 class="card-title">Contact Info</h4>
+                            <h4 class="card-title">Address</h4>
                         </div>
                         <div class="card-body">
                             <div class="row">
@@ -202,9 +230,10 @@
                                         <input
                                             type="text"
                                             id="line-1"
+                                            name="line-1"
                                             class="form-control"
                                             placeholder="Number and street"
-                                            name="line-1"
+                                            value="{{old('line-1')}}"
                                         />
                                     </div>
                                 </div>
@@ -214,10 +243,28 @@
                                         <input
                                             type="text"
                                             id="line-2"
+                                            name="line-2"
                                             class="form-control"
                                             placeholder="Apartment, suite, etc. (optional)"
-                                            name="line-2"
+                                            value="{{old('line-2')}}"
                                         />
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-12">
+                                    <div class="mb-1">
+                                        <label class="form-label" for="address-country">Country<strong><span
+                                                    class="text-danger">*</span></strong></label>
+                                        <select class="form-select select2" id="address-country"
+                                                name="address-country"></select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-12">
+                                    <div class="mb-1">
+                                        <label class="form-label" for="region">Region<strong><span
+                                                    class="text-danger">*</span></strong></label>
+                                        <select class="form-select select2" id="region" name="region">
+                                            <option id=""></option>
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="col-md-6 col-12">
@@ -226,9 +273,10 @@
                                         <input
                                             type="text"
                                             id="city"
+                                            name="city"
                                             class="form-control"
                                             placeholder="City"
-                                            name="city"
+                                            value="{{old('city')}}"
                                         />
                                     </div>
                                 </div>
@@ -238,9 +286,10 @@
                                         <input
                                             type="text"
                                             id="zip"
+                                            name="zip"
                                             class="form-control"
                                             placeholder="Zip Code"
-                                            name="zip"
+                                            value="{{old('zip')}}"
                                         />
                                     </div>
                                 </div>
@@ -249,7 +298,7 @@
                         </div>
                         <div class="card-footer">
                             <div class="col-12">
-                                <button type="reset" class="btn btn-primary me-1">Submit</button>
+                                <button type="submit" class="btn btn-primary me-1">Submit</button>
                                 <button type="reset" class="btn btn-outline-secondary">Reset</button>
                             </div>
                         </div>
@@ -265,9 +314,10 @@
     <!-- vendor files -->
     <script src="{{ asset(mix('vendors/js/pickers/flatpickr/flatpickr.min.js')) }}"></script>
     <script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/intl-tel-input/intlTelInput-jquery.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/intl-tel-input/utils.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/webui-popover/jquery.webui-popover.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/peership/js/forms/intl-tel-input/intlTelInput-jquery.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/peership/js/forms/intl-tel-input/utils.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/peership/js/forms/webui-popover/jquery.webui-popover.min.js')) }}"></script>
+    <script src="{{asset(mix('vendors/peership/js/forms/dependent-dropdown/dependent-dropdown.min.js'))}}"></script>
 @endsection
 @section('page-script')
     <!-- Page js files -->

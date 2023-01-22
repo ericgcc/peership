9cd7df85e4b098f39ee7736689242756b3ab01b0
ericgcc <ericgcc@gmail.com>
2023-01-22 14:13:02 -0500

    task: misc

--- resources/views/panels/styles.blade.php
+++ resources/views/panels/styles.blade.php
@@ -3,9 +3,9 @@
   <link rel="stylesheet" href="{{ asset(mix('vendors/css/vendors-rtl.min.css')) }}" />
 @else
   <link rel="stylesheet" href="{{ asset(mix('vendors/css/vendors.min.css')) }}" />
-  <link rel="stylesheet" href="{{asset(mix('fonts/fontawesome/css/fontawesome.min.css'))}}">
-  <link rel="stylesheet" href="{{asset(mix('fonts/fontawesome/css/light.min.css'))}}">
-  <link rel="stylesheet" href="{{asset(mix('fonts/fontawesome/css/solid.min.css'))}}">
+  <link rel="stylesheet" href="{{asset(mix('fonts/font-awesome/css/font-awesome.min.css'))}}">
+  <link rel="stylesheet" href="{{asset(mix('fonts/font-awesome/css/light.min.css'))}}">
+  <link rel="stylesheet" href="{{asset(mix('fonts/font-awesome/css/solid.min.css'))}}">
   <link rel="stylesheet" href="{{asset(mix('fonts/streamline/style.css'))}}">
 @endif
 
